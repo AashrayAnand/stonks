1772b748d05ba4e8e931e1b39716467dcfb05daa
Ozan Erdal <oerdal@uw.edu>
2020-03-25 03:25:15 -0700

    landing page for client side

---
+++ frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginPage from '../containers/LoginPage';
+import '../App.css';
+
+const App = () => {
+  return (
+    <div className='App d-flex'>
+      <LoginPage />
+    </div>
+  )
+};
+
+export default App;
